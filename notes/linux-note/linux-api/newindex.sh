aa58842af2e1b03f2a5d26c296f94fd1be389b9d
kuanghl <kuanghl1998@163.com>
2023-04-04 17:06:27 +0800

    .sh add

--- notes/linux-note/linux-api/newindex.sh
+++ notes/linux-note/linux-api/newindex.sh
@@ -1,3 +1,4 @@
+###2
 # #! /bin/bash
 # #每次push之前先运行此脚本
 # for num in $*
@@ -14,23 +15,6 @@
 #     sed -i ''${num}'i\'"${res}"'' README.md
 # done
 
-# ##2
-# #! /bin/bash
-# #每次push之前先运行此脚本
-# for num in $*
-# do
-#     all=$(ls docs|sort -n)
-#     line=$(echo "${all}"|grep -n "^${num}\..*")
-#     echo "${line}"
-#     title=`echo ${line}|cut -d . -f 2`
-#     declare -l url=${title}
-#     newline=$(echo ${line}| cut -d : -f 2)
-#     declare -i num=$(echo ${line}|cut -d : -f 1)
-#     res=$(echo "|${newline%%.*}|[$title](https://github.com/kuanghl/khl_project/tree/master/notes/linux-note/linux-api/${newline//' '/'%20'})|[C](https://github.com/starFalll/LeetCode/blob/master/algorithms/${newline//' '/'%20'})|")
-#     num=num+6
-#     sed -i ''${num}'i\'"${res}"'' README.md
-# done
-
 ###1
 #! /bin/bash
 #针对之前从未整理过的文件进行整理，请根据自己的目录结构修改
@@ -43,6 +27,6 @@ ls docs|sort -n|while read line
 do
     title=`echo ${line}|cut -d . -f 2`
     declare -l url=${title}
-    echo "|${num_i}|[${line%%.*}](https://github.com/kuanghl/khl_project/tree/master/notes/linux-note/linux-api/${line%%.*}.$title)|[C](https://github.com/kuanghl/khl_project/tree/master/notes/linux-note/linux-api/${line%%.*}.$title)|" >>README.md 
+    echo "|${num_i}|[${line%%.*}](https://github.com/kuanghl/khl_project/tree/master/notes/linux-note/linux-api/docs/${line%%.*}.$title)|[C](https://github.com/kuanghl/khl_project/tree/master/notes/linux-note/linux-api/docs/${line%%.*}.$title)|" >>README.md 
     let num_i+=1
 done
